fd17d585943f4fd7e328b79ddca554f1d5ccc811
alessiobortone2 <alessiobortone2@gmail.com>
2017-08-07 14:43:32 +0100

    First spec

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
-describe Oystercard do
+require 'oystercard.rb'
 
+describe Oystercard do
+  subject(:Oystercard) { described_class.new }
+  it "check newly initialized card has a default balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 end
